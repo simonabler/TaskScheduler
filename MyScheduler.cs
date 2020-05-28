66066349ba61f28d14ed91325563db6d4afe0f7e
Simon Abler <simon.abler@gmail.com>
2020-05-28 09:57:54 +0200

    Add feature to stop timer create intervall from now

--- MyScheduler.cs
+++ MyScheduler.cs
@@ -2,26 +2,59 @@ using System;
 
 public static class MyScheduler
 {
-    public static void IntervalInSeconds(int hour, int sec, double interval, Action task)
+    public static int IntervalInSeconds(int hour, int sec, double interval, Action task)
     {
-        interval = interval/3600;
-        SchedulerService.Instance.ScheduleTask(hour, sec, interval, task);
+        interval /= 3600;
+        return SchedulerService.Instance.ScheduleTask(hour, sec, interval, task);
     }
 
-    public static void IntervalInMinutes(int hour, int min, double interval, Action task)
+    public static int IntervalInSeconds(double interval, Action task)
     {
-        interval = interval/60;
-        SchedulerService.Instance.ScheduleTask(hour, min, interval, task);
+        interval /= 3600;
+        return SchedulerService.Instance.ScheduleTask(interval, task);
     }
 
-    public static void IntervalInHours(int hour, int min, double interval, Action task)
+    public static int IntervalInMinutes(int hour, int min, double interval, Action task)
     {
-        SchedulerService.Instance.ScheduleTask(hour, min, interval, task);
+        interval /= 60;
+        return SchedulerService.Instance.ScheduleTask(hour, min, interval, task);
     }
 
-    public static void IntervalInDays(int hour, int min, double interval, Action task)
+    public static int IntervalInMinutes(double interval, Action task)
     {
-        interval = interval * 24;
-        SchedulerService.Instance.ScheduleTask(hour, min, interval, task);
+        interval /= 60;
+        return SchedulerService.Instance.ScheduleTask(interval, task);
+    }
+
+    public static int IntervalInHours(int hour, int min, double interval, Action task)
+    {
+        return SchedulerService.Instance.ScheduleTask(hour, min, interval, task);
+    }
+   
+    public static int IntervalInHours(double interval, Action task)
+    {
+        return SchedulerService.Instance.ScheduleTask(interval, task);
+    }
+
+    public static int IntervalInDays(int hour, int min, double interval, Action task)
+    {
+        interval *= 24;
+        return SchedulerService.Instance.ScheduleTask(hour, min, interval, task);
+    }
+
+    public static int IntervalInDays(double interval, Action task)
+    {
+        interval *= 24;
+        return SchedulerService.Instance.ScheduleTask(interval, task);
+    }
+
+    public static void Stop()
+    {
+        SchedulerService.Instance.Stop();
+    }
+
+    public static void Stop(int timerIndex)
+    {
+        SchedulerService.Instance.Stop(timerIndex);
     }
 }
--- Services/SchedulerService.cs
+++ Services/SchedulerService.cs
@@ -5,13 +5,13 @@ using System.Threading;
 public class SchedulerService
 {
     private static SchedulerService _instance;
-    private List<Timer> timers = new List<Timer>();
-
-    private SchedulerService() { }
-
     public static SchedulerService Instance => _instance ?? (_instance = new SchedulerService());
 
-    public void ScheduleTask(int hour, int min, double intervalInHour, Action task)
+    private readonly Dictionary<int, Timer> _timers = new Dictionary<int, Timer>();
+
+    private SchedulerService() { }
+    
+    public int ScheduleTask(int hour, int min, double intervalInHour, Action task)
     {
         DateTime now = DateTime.Now;
         DateTime firstRun = new DateTime(now.Year, now.Month, now.Day, hour, min, 0, 0);
@@ -26,11 +26,63 @@ public class SchedulerService
             timeToGo = TimeSpan.Zero;
         }
 
+        return ScheduleTask(timeToGo, intervalInHour, task);
+    }
+
+    public int ScheduleTask(double intervalInHour, Action task)
+    {
+        return ScheduleTask(TimeSpan.Zero, intervalInHour, task);
+    }
+
+    public int ScheduleTask(TimeSpan timeToGo, double intervalInHour, Action task)
+    {
         var timer = new Timer(x =>
         {
             task.Invoke();
         }, null, timeToGo, TimeSpan.FromHours(intervalInHour));
+        return AddTimer(timer);
+    }
 
-        timers.Add(timer);
+    public void Stop()
+    {
+        foreach (var timer in _timers.Values)
+        {
+            StopTimer(timer);
+        }
+        _timers.Clear();
+    }
+
+    public void Stop(int index)
+    {
+        if (!_timers.ContainsKey(index))
+            return;
+
+        StopTimer(_timers[index]);
+        _timers.Remove(index);
+    }
+
+    private void StopTimer(Timer timer)
+    {
+        timer.Change(0, Timeout.Infinite);
+        timer.Dispose();
+    }
+
+    private int AddTimer(Timer timer)
+    {
+        int hash = RandomNumber(int.MinValue, int.MaxValue);
+
+        while (_timers.ContainsKey(hash))
+        {
+            hash = RandomNumber(int.MinValue, int.MaxValue);
+        }
+
+        _timers.Add(hash, timer);
+        return hash;
+    }
+
+    private int RandomNumber(int min, int max)
+    {
+        Random random = new Random();
+        return random.Next(min, max);
     }
 }
